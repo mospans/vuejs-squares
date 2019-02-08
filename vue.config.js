9ac73cfe211c03a6356813676e8ecd30610115f4
Sergey Mospan <s@mospan.ru>
2019-02-08 16:29:14 +0300

    Change favicon properties

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   lintOnSave: false,
+  pwa: {
+    favicon32: 'favicon.ico',
+    favicon16: 'favicon.ico',
+    appleTouchIcon: 'img/icons/icon-152x152.png',
+    maskIcon: null,
+    msTileImage: 'img/icons/icon-144x144.png',
+  },
 };
